19c7ad41e2f44696804e671e0ed2779fffe2b5b7
unknown <tuhinrahmna48@gmail.com>
2026-07-22 00:47:57 +0600

    fix: change default port to 3000 and update CORS origins

--- src/main.ts
+++ src/main.ts
@@ -7,15 +7,16 @@ async function bootstrap() {
   });
   app.enableCors({
     origin: [
+      'https://halo.codevionix.com',
       'https://taskpro.codevionix.com',
       'http://localhost:3000',
-      'http://localhost:5173'
+      'http://localhost:5173',
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   }); // Enable CORS for the frontend
   app.setGlobalPrefix('api'); // Add /api prefix to all routes
-  const port = process.env.PORT ?? 5000;
+  const port = process.env.PORT ?? 3000;
   await app.listen(port, '0.0.0.0');
 }
 bootstrap();
